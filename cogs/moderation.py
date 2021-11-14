ec81ccc9bc4394370ec6788fe7f1c15f5d7da4b7
meesvw <mees011@hotmail.nl>
2021-11-14 21:17:51 +0100

    moderation.py bug fix

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -136,7 +136,7 @@ class Moderation(commands.Cog):
     @commands.has_any_role(
         669181460124532736, 697198873495470090, 669371769672564776, 705844874590552365, 750673616584048741
     )
-    async def welkom(self, ctx, user: discord.Member):
+    async def welkom(self, ctx: SlashContext, user: discord.Member):
         await ctx.message.delete()
 
         add_role = ctx.guild.get_role(668825700798693377)
