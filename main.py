a7e4590f0ac76fadee2615d2bb372f6599c3f53d
meesvw <mees011@hotmail.nl>
2022-07-21 14:45:39 +0200

    dynamic update

--- main.py
+++ main.py
@@ -53,11 +53,11 @@ async def on_member_join(member):
         member.guild.get_role(685607372428804104),
         reason='Joined server'
     )
-    # 669161755972206629 = regels
-    # 935961075407523900 = roles
+    regels_channel = 669161755972206629
+    roles_channel = 935961075407523900
     await bot.get_channel(722771390092279819).send(embed=await embeds.join_log(member))
     await bot.get_channel(696859692684541983).send(
-        f'Welkom {member.mention}! Lees alvast de <#669161755972206629> en selecteer je <#935961075407523900>, '
+        f'Welkom {member.mention}! Lees alvast de <#{regels_channel}> en selecteer je <#{roles_channel}>, '
         f'een <@&669371769672564776> komt je zo snel mogelijk helpen! <:KellyHappyMood:720436790913269802>'
     )
 
